6789bd808827225d7246630da1b613f575136a9c
gadosanjos <147287432+gadosanjos@users.noreply.github.com>
2026-09-14 17:19:28 -0700

    skeleton

---
+++ src/PathLiner.Console/Program.cs
@@ -0,0 +1,3 @@
+﻿using PathLiner;
+
+Console.WriteLine(PathLinerTest.Hello());
---
+++ src/PathLiner/PathLinerTest.cs
@@ -0,0 +1,11 @@
+// src/PathLiner/PathLinerTest.cs
+
+namespace PathLiner;
+
+public class PathLinerTest
+{
+    public static string Hello()
+    {
+        return "Hello from the PathLiner library!";
+    }
+}
